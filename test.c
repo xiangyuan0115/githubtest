344e287785285afde4fce5a0e35ad1b7741b70c9
xiangyuan0115 <78306280@qq.com>
2019-11-24 23:12:42 -0800

    add test

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	#	 FileName	: test.c
+	#	 Author		: dongjianqiang 
+	#	 Email		: 78306280@qq.com 
+	#	 Created	: Sun 24 Nov 2019 11:11:51 PM PST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("hello world\n");
+	return 0;
+}
