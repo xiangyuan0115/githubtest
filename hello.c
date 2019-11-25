f9988e8b525c0570ae670e9637d7f639682fbaa3
xiangyuan0115 <78306280@qq.com>
2019-11-24 23:03:19 -0800

    modified hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	#	 FileName	: hello.c
+	#	 Author		: dongjianqiang 
+	#	 Email		: 78306280@qq.com 
+	#	 Created	: Sun 24 Nov 2019 11:00:19 PM PST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    printf("hello world\n");
+	return 0;
+}
